8a5849538021a9dbf55f65e28a77b41b61eaf7a9
Cole Henson <colehenson95@gmail.com>
2021-08-31 22:16:26 -0600

    feat: add greeting method

---
+++ lib/greeting.rb
@@ -0,0 +1,15 @@
+def greeting
+  puts "Welcome to MASTERMIND!
+  Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  response = gets.chomp
+  if response == 'P' || response == 'p'
+    puts 'This is a placeholder'
+  elsif response == 'I' || response ==  'i'
+    puts 'here are the instructions'
+  elsif response == 'Q' || response == 'q'
+    puts greeting
+  else puts 'Please select one of the options above'
+  end
+end
+
+greeting
--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -3,8 +3,15 @@ require './lib/mastermind'
 
 RSpec.describe Game do
   it 'exists' do
-  game = Game.new
+  game = Game.new(secret)
+  secret = ['R', 'R', 'R', 'R']
 
   expect(game).to be_an_instance_of(Game)
+  end
+
+  xit 'has readable attributes' do
+    game = Game.new(['R', 'R', 'G', 'Y'])
+
+    expect Game.new.to eq([''])
   end
 end
