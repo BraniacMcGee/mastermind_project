468382dec5c8e4609fea62d86dcfd3d55b1482ac
Christian Valesares <cvalesares@gmail.com>
2021-09-04 15:36:30 -0400

    feat: add compare_guess method and fixes to guess_spec

--- lib/guess.rb
+++ lib/guess.rb
@@ -3,33 +3,32 @@ class Guess
               :convert
 
   def initialize(guess)
-    @guess = gets.chomp
+    @guess = guess
     @secret = Secret.new(['R', 'G', 'B', 'Y'])
-
-  end
-
-  def convert
-    @guess.split('')
   end
 
   def too_short?
-  true == @guess.convert.count < 4
+  true == @guess.split('').count < 4
   end
 
   def too_long?
-    true == @guess.convert.count > 4
+    true == @guess.split('').count > 4
   end
 
   def error_message
     if too_long? == true
-      p 'Your guess is too long. All guesses must be 4 characters.
-      Please try again.'
-    else too_short? == true
-      p 'Your guess is too short. All guesses must be 4 characters.
-      Please try again.'
+      p 'Your guess is too long. All guesses must be 4 characters. Please try again.'
+    elsif too_short? == true
+      p 'Your guess is too short. All guesses must be 4 characters. Please try again.'
     end
   end
-end
 
-guess = Guess.new('YRGB')
-guess.too_long?
+  def compare_guess(secret)
+    #use @guess.eql?(@secret) to test win
+    #comparing index tests how many in the right spot
+    #
+  end
+end
+#
+# guess = Guess.new('YRGB')
+# guess.too_long?
--- spec/guess_spec.rb
+++ spec/guess_spec.rb
@@ -34,4 +34,29 @@ describe 'Guess' do
     @secret = ['Y', 'G', 'B', 'R']
     expect(guess.too_short?).to eq(true)
     end
+
+  it 'delivers an error' do
+    guess = Guess.new('RGBY')
+    @secret = ['Y', 'G', 'B', 'R']
+
+  expect(guess.error_message).to be nil
+
+    guess = Guess.new('RGBYY')
+    @secret = ['Y', 'G', 'B', 'R']
+
+  expect(guess.error_message).to eq('Your guess is too long. All guesses must be 4 characters. Please try again.')
+
+    guess = Guess.new('RGB')
+    @secret = ['Y', 'G', 'B', 'R']
+
+  expect(guess.error_message).to eq('Your guess is too short. All guesses must be 4 characters. Please try again.')
   end
+
+  xit 'compares guess to secret' do
+    guess = Guess.new('RGBY')
+    @secret = ['Y', 'G', 'B', 'R']
+    guess1  = ['B', 'G', 'Y', 'R']
+    # 'rrgb' has 3 of the correct elements with 2 in the correct positios
+    #you've taken 1 guess
+  end
+end
